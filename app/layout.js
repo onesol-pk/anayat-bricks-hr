8a7c1797c3f38feba3c1b3d73298637c67a4b5f2
onesol-pk <uk.onesol@gmail.com>
2026-04-21 01:57:02 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Anayat Sons Bricks",
+  description: "HR & Wage System",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
