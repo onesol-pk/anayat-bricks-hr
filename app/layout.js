a7c77e3c84d262cc21bd1bd2593260f2fabc841b
onesol-pk <uk.onesol@gmail.com>
2026-04-22 01:04:11 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
 import "./globals.css"
 import { Inter, Poppins } from "next/font/google"
-const inter = Inter({ subsets: ["latin"] })
-const poppins = Poppins({ weight: ["400", "600"], subsets: ["latin"] })
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+  variable: "--font-poppins",
+})
+
 export const metadata = {
   title: "Anayat Sons Bricks",
   description: "HR & Wage System",
@@ -10,10 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.variable} ${poppins.variable}`}>
         {children}
       </body>
     </html>
   )
 }
-
