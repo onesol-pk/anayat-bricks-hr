61e6234a6b3e20a76cf09065cf740c69603c56b2
onesol-pk <uk.onesol@gmail.com>
2026-06-04 10:53:58 +0500

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -364,6 +364,10 @@ export default function AdminDashboard() {
             <p className="text-xs uppercase tracking-[0.25em] text-gray-400">
               Anayat Sons Bricks
             </p>
+              <div class="address">
+            248 R.B, Dalowal,<br />
+            Samundri Road, Faisalabad
+          </div>
             <h1 className="text-lg font-bold">Dashboard</h1>
           </div>
 
